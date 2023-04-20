7d61c7689de793afda19bcde782fcae89e966a6f
Andre Castillo <a01254180@itesm.mx>
2023-04-19 22:39:31 -0700

    Added routes of implemented controllers to index.js

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const role_routes = require("./src/routes/role-routes");
 const office_routes = require("./src/routes/office-routes");
 const status_viatico_request_routes = require("./src/routes/status-viatico-request-routes");
 const status_expense_report_routes = require("./src/routes/status-expense-report-routes");
+const status_refund_routes = require("./src/routes/status-refund-routes");
+const accound_routes = require("./src/routes/account-routes");
+const notification_routes = require("./src/routes/notification-routes");
+const project_routes = require("./src/routes/project-routes");
+const viatico_request_routes = require("./src/routes/viatico-request-routes");
+const viatico_routes = require("./src/routes/viatico-routes");
 const type_expense_routes = require("./src/routes/type-expense-routes");
 const home_routes = require("./src/routes/home-routes");
 
@@ -29,6 +35,12 @@ app.use("/offices", office_routes);
 app.use("/status_viatico_request", status_viatico_request_routes);
 app.use("/status_expense_report", status_expense_report_routes);
 app.use("/type_expense", type_expense_routes);
+app.use("/status_refund", status_refund_routes);
+app.use("/accounts", accound_routes);
+app.use("/notifications", notification_routes);
+app.use("/projects", project_routes);
+app.use("/viatico_request", viatico_request_routes);
+app.use("/viaticos", viatico_routes);
 app.use(home_routes);
 
 app.all('*', (req, res) => {
