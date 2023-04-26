9a5cb2489c45b78abe529d96faad7bc8a089d02e
m <m@tag.local>
2023-04-26 11:42:03 -0700

    Changed file names

--- src/routes/LoginRoutes.js
+++
@@ -1,9 +0,0 @@
-let express = require("express");
-let notification_controller = require("../controllers/LoginController");
-import {Login} from '../controllers/LoginController';
-
-let router = express.Router();
-
-router.post('/', Login);
-
-module.exports = router;
