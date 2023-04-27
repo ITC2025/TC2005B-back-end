0f3ba2c365b5c011363dfa5839c71057d662edfb
Andre Castillo <a01254180@itesm.mx>
2023-04-26 17:57:16 -0700

    Added controller to get viatico records for a PM

--- src/routes/viaticos-records-routes.js
+++ src/routes/viaticos-records-routes.js
@@ -6,6 +6,7 @@ let router = express.Router();
 
 router.get("/:id", viaticos_record_controller.viaticos_record_get_by_id);
 router.get("/", viaticos_record_controller.viaticos_record_index);
+router.get("/pm/:id", viaticos_record_controller.viaticos_record_get_by_pm_id);
 router.patch("/:id", viaticos_record_controller.viaticos_record_update);
 router.post("/", viaticos_record_controller.viaticos_record_create);
 router.delete("/:id", viaticos_record_controller.viaticos_record_delete);
