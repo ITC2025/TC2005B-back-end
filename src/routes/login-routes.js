72fde3d216bd9af69812ad0f0269fbdf50919fe5
Andre Castillo <a01254180@itesm.mx>
2023-04-28 14:48:34 -0700

    Added logout request controller

--- src/routes/login-routes.js
+++ src/routes/login-routes.js
@@ -4,6 +4,7 @@ const login_controller  = require("../controllers/login-controller");
 const router = express.Router();
 
 router.post('/', login_controller.login_post);
+router.post('/logout', login_controller.logout_post);
 
 router.get('/', (req, res) => {
 	res.send("Login Screen!!!");
