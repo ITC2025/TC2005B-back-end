dacb72ee10ab8f7b65325e75b0b04af9075130a4
Jaza2401 <alex.jauregui.zarate@gmail.com>
2023-05-01 21:18:37 -0700

    - Controlador de la pantalla solicitud de admin

--- src/routes/viatico-request-routes.js
+++ src/routes/viatico-request-routes.js
@@ -4,6 +4,7 @@ const viatico_request_controller = require("../controllers/viatico-request-contr
 
 const router = express.Router();
 
+
 router.get("/:id", viatico_request_controller.viatico_request_get_by_id);
 router.get("/pm/:id", viatico_request_controller.viatico_request_get_by_pm_id);
 router.get("/", viatico_request_controller.viatico_request_index);
@@ -11,6 +12,10 @@ router.patch("/:id", viatico_request_controller.viatico_request_update);
 router.post("/", viatico_request_controller.viatico_request_create);
 router.delete("/:id", viatico_request_controller.viatico_request_delete);
 
-router.post("/solicitar",viatico_request_controller.solicitar_viatico)
+
+router.get("/admin", viatico_request_controller.project_admin);
+
+router.get("/", viatico_request_controller.viatico_request_index);
+router.post("/solicitar",viatico_request_controller.solicitar_viatico);
 
 module.exports = router;
