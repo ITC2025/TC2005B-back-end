b7d92316ed067d0175823768e6c3d0aeee904252
Andre Castillo <a01254180@itesm.mx>
2023-05-02 19:50:42 -0700

    Added back controller to get viatico request by user ID

--- src/controllers/viatico-request-controller.js
+++ src/controllers/viatico-request-controller.js
@@ -102,6 +102,23 @@ module.exports.viatico_request_get_by_project = (req, res) => {
 	});
 };
 
+module.exports.viatico_request_get_by_user_id = (req, res) => {	
+	res.set('Access-Control-Allow-Origin', ['http://localhost:3000']);
+	db.SolicitudViaticos.findAll({
+		where: {
+			ID_empleado : req.params.id
+		},
+		include: [{
+			model: db.Proyectos
+		},
+		{
+			model: db.StatusSolicitudViaticos
+		}]
+	}).then((result) => {
+		res.send(result);
+	});
+};
+
 module.exports.viatico_request_get_by_pm_id = (req, res) => {	
 	res.set('Access-Control-Allow-Origin', ['http://localhost:3000']);
 	db.SolicitudViaticos.findAll({
