8bf24d60c50e54c349929c56d624cdafab822e9d
mazomenos31 <gustbetancourt@hotmail.com>
2023-05-02 18:14:22 -0700

    added features for expenses smart delete controller
    - exclusion for selected status
    
    - added route for img

--- src/controllers/expense-table-controller.js
+++ src/controllers/expense-table-controller.js
@@ -7,6 +7,7 @@
 
 const { stat } = require('fs');
 let db = require('../models')
+const { Op } = require("sequelize");
 
 module.exports.side_info = (req, res) => {	
 	res.set('Access-Control-Allow-Origin', ['http://localhost:3000']);
@@ -35,19 +36,30 @@ module.exports.expense_table = (req, res) => {
 			where: {ID_solicitud_viatico : req.params.id},
 		},
 		{
+			model: db.StatusReporteGastos,
+			where: {
+				ID_status_reporte_gasto: {
+				[Op.not]: 5
+				}
+			}
+		},
+		{
 			model: db.TipoGastos
 		}
 	]
 	}).then((result) => {
+		console.log(result);
 		const gastos = result.map((gasto) => {
 			return {
 				id: gasto.ID_reporte_gasto,
 				fecha: gasto.fecha,
 				tipo: gasto.TipoGasto.descripcion,
 				concepto: gasto.concepto,
-				total: gasto.monto
+				total: gasto.monto,
+				status: gasto.StatusReporteGasto.ID_status_reporte_gasto
 			}
 		})
+		console.log(gastos);
 		res.send(gastos);
 	});
 
@@ -55,9 +67,29 @@ module.exports.expense_table = (req, res) => {
 
 module.exports.sum = (req, res) => {	
 	res.set('Access-Control-Allow-Origin', ['http://localhost:3000']);
-	db.ReporteGastos.sum('monto', { where : {ID_solicitud_viatico : req.params.id}})
+	db.ReporteGastos.sum('monto', 
+	{ where : {ID_solicitud_viatico : req.params.id, ID_status_reporte_gasto: {
+		[Op.not]: 5
+		}}, 
+	})
 	.then((resultado => {
 		res.send({monto : resultado})
 	}))
+
+};
+
+module.exports.expense_image = (req, res) => {	
+	res.set('Access-Control-Allow-Origin', ['http://localhost:3000']);
+	db.ReporteGastos.findAll({
+		where: {ID_reporte_gasto : req.params.id},
+	}).then((result) => {
+		const gastos = result.map((gasto) => {
+			return {
+				imagen: gasto.imagen
+			}
+		})
+		console.log(gastos);
+		res.send(gastos);
+	});
 
 };
